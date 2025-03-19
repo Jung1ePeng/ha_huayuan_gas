a9f9786e9e4085820d2082e106e34482c2f0880f
Jung1ePeng <pengjiang0414@gmail.com>
2025-03-19 22:00:21 +0800

    Update __init__.py

--- custom_components/huayuan_gas/__init__.py
+++ custom_components/huayuan_gas/__init__.py
@@ -16,7 +16,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     coordinator = HuayuanGasBalanceCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
